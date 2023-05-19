7abd38162e747bcf296a9f3ff89c07ef617d53e1
atikur-24 <atikurrahman7223@gmail.com>
2023-05-19 22:54:48 +0600

    added attractive toys gallery section

---
+++ src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,10 @@
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>Thia ia page</h2>
+        </div>
+    );
+};
+
+export default Blogs;
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -2,7 +2,7 @@
 const ErrorPage = () => {
     return (
         <div>
-            <h3 className="text-3xl">404 page</h3>
+            <h3 className="text-3xl">404 page</h3> 
         </div>
     );
 };
