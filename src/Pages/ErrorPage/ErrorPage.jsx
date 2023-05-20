c83dec5669bfc74a68d417dd1947c07ad17aa7a2
atikur-24 <atikurrahman7223@gmail.com>
2023-05-21 00:56:38 +0600

    added error page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,22 @@
-
+import { Link, useRouteError } from 'react-router-dom';
+import Lottie from "lottie-react";
+import errorImg from "../../assets/error/error-page-not-found-confused-robot.json"
 const ErrorPage = () => {
+    const {error} = useRouteError();
+
     return (
-        <div>
-            <h3 className="text-3xl">404 page</h3> 
-        </div>
+        <section className='my-container flex flex-col lg:flex-row items-center justify-between'>
+            <div className='w-1/2'>
+                <Lottie className='w-50' animationData={errorImg} loop={true} />
+            </div>
+            <div className='space-y-10 mr-16 lg:mr-20'>
+                <h2 className='text-4xl font-bold'>Page Not Found</h2>
+                <h5 className='font-semibold'>{error?.message}</h5>
+                <button>
+                <Link to='/' className='my-btn'> Back to Home page</Link>
+                </button>
+            </div>
+        </section>
     );
 };
 
